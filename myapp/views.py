b64bbd77c8c717f8e6d843ab3a02ab1cd815fd6f
Tamim <tamimvaiya.123@gmail.com>
2025-08-18 13:25:40 +0600

    Displayed List of Rooms

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import ChatRoom
 
 # Create your views here.
 def index(request):
-    return render(request, 'myapp/index.html')
+    chatrooms = ChatRoom.objects.all()
+    return render(request, 'myapp/index.html', {'chatrooms':chatrooms})
