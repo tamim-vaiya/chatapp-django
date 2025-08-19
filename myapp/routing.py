989247a3f56c77737a9247fb7e8cb2f54364b5e3
Tamim <tamimvaiya.123@gmail.com>
2025-08-19 16:45:53 +0600

    Created a Socket

--- myapp/routing.py
+++ myapp/routing.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .consumers import ChatConsumer
 
 websocket_urlpatterns = [
-    path('ws/<str:room_name>', ChatConsumer.as_asgi()),
+    path('ws/<str:room_name>/', ChatConsumer.as_asgi()),
 ]
